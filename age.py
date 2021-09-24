9b490753e082370b1e54390137b40a3843e4a22c
crestel-ong <crestel.ong@mths.ca>
2021-09-24 17:34:13 +0000

    good

---
+++ age.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: Sept 2021
+# This is the Dating program in python
+
+
+def main():
+    # this function tells you if you with in the age range to date
+
+    # input
+    integer_as_string = input("Please enter your age: ")
+
+    try:
+        # convert sring to integer
+        user_age = int(integer_as_string)
+
+        # process and output
+        if user_age > 25 and user_age < 40:
+            print("You are accepted to date my grandchild.")
+        elif user_age < 25:
+            print("You are too young!")
+        elif user_age > 40:
+            print("You are too old!")
+    except Exception:
+        print("{0} is not an integer.".format(integer_as_string))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
